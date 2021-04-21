7ec83460f0abac362538f93909e1eb08834568c1
abdbuzurg <thesarboz@gmail.com>
2021-04-21 22:04:51 +0500

    forever token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,5 +2,5 @@ import { sign } from "jsonwebtoken";
 import { User } from "../entity/User";
 
 export const createAccessToken = (user: User) => {
-  return sign({ userId: user.id }, "secret", { expiresIn: "9999y"})
+  return sign({ userId: user.id }, "secret")
 }
