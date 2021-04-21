f1a1c2907b6fdb1a6c64892b8b700280ffdee78e
abdbuzurg <thesarboz@gmail.com>
2021-04-21 16:17:19 +0500

    added new token lifespan

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,5 +2,5 @@ import { sign } from "jsonwebtoken";
 import { User } from "../entity/User";
 
 export const createAccessToken = (user: User) => {
-  return sign({ userId: user.id }, "secret", { expiresIn: "9999m"})
+  return sign({ userId: user.id }, "secret", { expiresIn: "9999y"})
 }
