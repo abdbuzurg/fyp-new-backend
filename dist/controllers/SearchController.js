406b27c3f3b2ae40de6cc2cb194f7c50df423a90
abdbuzurg <thesarboz@gmail.com>
2021-04-28 00:21:00 +0500

    new search engine

---
+++ dist/controllers/SearchController.js
@@ -0,0 +1,45 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.search = void 0;
+const ClientFeed_1 = require("src/entity/ClientFeed");
+const DriverFeed_1 = require("src/entity/DriverFeed");
+const errorResponse_1 = require("src/utils/errorResponse");
+const search = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const initialLocation = req.body.initialLocation;
+        const finalLocation = req.body.finalLocation;
+        const isClientFeed = Boolean(req.body.isClientFeed);
+        let result;
+        if (isClientFeed) {
+            const both = yield ClientFeed_1.ClientFeed.find({ where: { destinationFrom: initialLocation, destinationTo: finalLocation } });
+            const basedOnInitialLocation = yield ClientFeed_1.ClientFeed.find({ where: { destinationFrom: initialLocation } });
+            const basedOnFinalLocation = yield ClientFeed_1.ClientFeed.find({ where: { destinationTo: finalLocation } });
+            result = Object.assign(Object.assign(Object.assign({}, both), basedOnInitialLocation), basedOnFinalLocation);
+        }
+        else {
+            const both = yield DriverFeed_1.DriverFeed.find({ where: { destinationFrom: initialLocation, destinationTo: finalLocation } });
+            const basedOnInitialLocation = yield DriverFeed_1.DriverFeed.find({ where: { destinationFrom: initialLocation } });
+            const basedOnFinalLocation = yield DriverFeed_1.DriverFeed.find({ where: { destinationTo: finalLocation } });
+            result = Object.assign(Object.assign(Object.assign({}, both), basedOnInitialLocation), basedOnFinalLocation);
+        }
+        res.send({
+            success: true,
+            message: "Search completed",
+            data: result,
+        });
+    }
+    catch (error) {
+        res.send(errorResponse_1.errorHandler(error));
+    }
+});
+exports.search = search;
+//# sourceMappingURL=SearchController.js.map
--- dist/routes/routers.js
+++ dist/routes/routers.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const clientFeedRoutes_1 = __importDefault(require("./clientFeedRoutes"));
 const driverFeedRouter_1 = __importDefault(require("./driverFeedRouter"));
+const searchRoutes_1 = __importDefault(require("./searchRoutes"));
 const userRouter_1 = __importDefault(require("./userRouter"));
 const allRoutesV1 = express_1.Router();
 allRoutesV1.use('/user/', userRouter_1.default);
 allRoutesV1.use('/driver/', driverFeedRouter_1.default);
 allRoutesV1.use('/client/', clientFeedRoutes_1.default);
+allRoutesV1.use('/search/', searchRoutes_1.default);
 exports.default = allRoutesV1;
 //# sourceMappingURL=routers.js.map
