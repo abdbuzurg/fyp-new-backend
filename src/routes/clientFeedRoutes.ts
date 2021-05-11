78a693099e4b429d572ebacb204daaf6d43e0a85
abdbuzurg <thesarboz@gmail.com>
2021-05-12 02:41:25 +0500

    123

--- src/routes/clientFeedRoutes.ts
+++ src/routes/clientFeedRoutes.ts
@@ -8,6 +8,6 @@ clientFeedRouter.get("/", Auth, ClientFeedController.getAll);
 clientFeedRouter.post("/", Auth, ClientFeedController.create);
 clientFeedRouter.put("/:id", Auth, ClientFeedController.update);
 clientFeedRouter.delete("/:id", Auth, ClientFeedController.deleteFeed);
-clientFeedRouter.get("history/", Auth, ClientFeedController.history)
+clientFeedRouter.get("/history", Auth, ClientFeedController.history)
 
 export default clientFeedRouter;
